df7aba86f57e266679f7ecfa06f8cd43d163418a
jacob-craffey <craffeyj@mail.gvsu.edu>
2017-05-24 13:12:50 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Displays the image from the file chooser.
 function handleImage(e){
     var reader = new FileReader();
     reader.onload = function(event){
@@ -12,6 +13,7 @@ function handleImage(e){
     reader.readAsDataURL(e.target.files[0]);
 }
 
+// Gives the image a glitch effect
 function distort(newVal) {
     imgData = getImgData();
     var random = (Math.floor(Math.random() * 2500) + 1) * 4;
@@ -25,16 +27,16 @@ function distort(newVal) {
         var y = Math.floor((i / 4) / canvas.width);
         
         for (var j = 0; j < newVal; j++) {
-            imgData.data[((y * (imgData.width * 4)) + ((x * 4) + j))] = imgData.data[(((y - newVal) * (imgData.width * 4)) + ((x * 4) + j))];
-            imgData.data[((y * (imgData.width * 4)) + ((x * 4) + j)) + 1] = imgData.data[(((y - newVal) * (imgData.width * 4)) + ((x * 4) + j + 1))];
-            imgData.data[((y * (imgData.width * 4)) + ((x * 4) + j)) + 2] = imgData.data[(((y - newVal) * (imgData.width * 4)) + ((x * 4) + j + 2))];
-            imgData.data[((y * (imgData.width * 4)) + ((x * 4) + j)) + 3] = imgData.data[(((y - newVal) * (imgData.width * 4)) + ((x * 4) + j + 3))];
+            imgData.data[((y * (imgData.width * 4)) + ((x * 4) + j))] = imgData.data[((Math.abs(y - newVal) * (imgData.width * 4)) + ((x * 4) + j))];
+            imgData.data[((y * (imgData.width * 4)) + ((x * 4) + j)) + 1] = imgData.data[((Math.abs(y - newVal) * (imgData.width * 4)) + ((x * 4) + j + 1))];
+            imgData.data[((y * (imgData.width * 4)) + ((x * 4) + j)) + 2] = imgData.data[((Math.abs(y - newVal) * (imgData.width * 4)) + ((x * 4) + j + 2))];
+            imgData.data[((y * (imgData.width * 4)) + ((x * 4) + j)) + 3] = imgData.data[((Math.abs(y - newVal) * (imgData.width * 4)) + ((x * 4) + j + 3))];
         }
     }
     ctx.putImageData(imgData, 0, 0);
 }
 
-
+// Inverts the image's pixel color
 function invert(){
     imgData = getImgData();
     var invertNum = 255;
@@ -48,8 +50,11 @@ function invert(){
     ctx.putImageData(imgData, 0, 0);
 }
 
+/* Sorts the image's pixels from brightest to darkest.  
+DISCLAIMER: The bubblesort algorithm is not ideal for this an am working
+on implementing a more efficient algorithm such as merge-sort. */
 function bubbleSort() {
-    imgData = ctx.getImageData(0, 0, img.height. img.width);
+    imgData = ctx.getImageData(0, 0, canvas.height, canvas.width);
     
     for (var n = 0; n < imgData.data.length; n+=4) {
         for (var i = 0; i < imgData.data.length - n - 4; i+=4) {
@@ -79,14 +84,18 @@ function bubbleSort() {
     
 }
 
+// Returns the image to its original state.
 function erase() {
     ctx.drawImage(img, 0 , 0);
 }
 
+
+// Returns the imageData for image.
 function getImgData() {
     var imgData = ctx.getImageData(0, 0, parseInt(img.height)*2, parseInt(img.width)*2);
     return imgData;
 }
+
 
 var imageLoader = document.getElementById('imageLoader');
 imageLoader.addEventListener('change', handleImage, false);
